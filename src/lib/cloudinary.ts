286fa48526c5de5cc2392ec3eabc15b7a22f75ce
Buinov Vadym <karabincom@gmail.com>
2026-03-20 18:25:48 +0200

    feat: add image protection helper and setup cloudinary transformations

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -15,4 +15,20 @@ cloudinary.config({
   secure: true,
 });
 
+export const getProtectedImageUrl = (publicId: string) => {
+  return cloudinary.url(publicId, {
+    transformation: [
+      { width: 1600, crop: 'limit', quality: 'auto', fetch_format: 'auto' },
+      {
+        overlay: 'lithium_placeholder',
+        opacity: 20,
+        width: '0.5',
+        flags: 'relative',
+        gravity: 'center',
+      },
+    ],
+    secure: true,
+  });
+};
+
 export default cloudinary;
