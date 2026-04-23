d16b15d5d34244ebf15a200ddd07ab4f9f11226c
Buinov Vadym <karabincom@gmail.com>
2026-04-23 12:44:02 +0300

    feat: implement global layout with Burger, add projects catalog and individual project routing

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,5 @@
 import { prisma } from '@/lib/prisma';
 import MainSlider from './../../components/home/MainSlider';
-import Burger from '@/components/layout/Burger';
 
 export default async function HomePage() {
   const photos = await prisma.photo.findMany({
@@ -14,14 +13,12 @@ export default async function HomePage() {
   if (photos.length === 0) {
     return (
       <main className="h-screen flex items-center justify-center bg-black text-white">
-        <Burger />
         <p>В базе нет фото. Загрузи их в админке, привязав к проекту!</p>
       </main>
     );
   }
   return (
     <main>
-      <Burger />
       <MainSlider photos={photos} />
     </main>
   );
